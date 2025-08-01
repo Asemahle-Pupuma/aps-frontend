c6923273d55bd2a4c5ca1a2e8caf6f85a43a0352
Asemahle-Pupuma <asemahlepupuma@gmail.com>
2025-08-01 09:46:16 +0200

    Update supabaseClient.js

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -1,1 +1,8 @@
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://your-project-id.supabase.co';
+const supabaseAnonKey = 'your-anon-key'; // safe to use in frontend
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
